4cb242ed89222da5eb25cab0d864f8c64a1c4622
jnh01 <44010028+jnh01@users.noreply.github.com>
2020-08-26 19:28:13 +0800

    Added exported data for ICAMSxtra
    "Target_pooled", "Reverse_pooled", "Target", "Reverse"

---
+++ R/data.R
@@ -0,0 +1,57 @@
+#' Transcription bias of indels classified into 115 categories (pyrimidine)
+#'
+#' This data is designed to be used as an example in function \cr
+#' \code{\link{ID115_PlotTransBias}} and \code{\link{ID115_PlotTransBiasToPdf}}.
+#'
+#' @format A \code{vector} which contains the 115 categories of indel events, standardised 
+#' to pyrimidine format.
+#'   
+#' @name Target
+NULL
+
+#' @rdname Target
+"target"
+
+#' Transcription bias of indels classified into 115 categories (purine)
+#'
+#' This data is designed to be used as an example in function \cr
+#' \code{\link{ID115_PlotTransBias}} and \code{\link{ID115_PlotTransBiasToPdf}}.
+#'
+#' @format A \code{vector} which contains the 115 categories of indel events, but in purine
+#' format
+#'   
+#' @name Reverse
+NULL
+
+#' @rdname Reverse
+"reverse"
+
+#' Transcription bias of indels classified into 13 categories (pyrimidine)
+#'
+#' This data is designed to be used as an example in function \cr
+#' \code{\link{ID115_PlotTransBias}} and 
+#' \code{\link{ID115_PlotTransBias}} 
+#'
+#' @format A \code{vector} which contains the 13 categories of indel events, standardised 
+#' to pyrimidine format.
+#'   
+#' @name Target_pooled
+NULL
+
+#' @rdname Target_pooled
+"target_pooled"
+
+#' Transcription bias of indels classified into 13 categories (purine)
+#'
+#' This data is designed to be used as an example in function \cr
+#' \code{\link{ID115_PlotTransBias}} and 
+#' \code{\link{ID115_PlotTransBias}} when \code{pool = TRUE}.
+#'
+#' @format A \code{vector} which contains the 13 categories of indel events, standardised 
+#' to purine format.
+#'   
+#' @name Reverse_pooled
+NULL
+
+#' @rdname Reverse_pooled
+"reverse_pooled"
