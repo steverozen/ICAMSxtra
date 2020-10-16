b2a596d374896a5b9e0700bece7462b9187a3b20
jnh01 <44010028+jnh01@users.noreply.github.com>
2020-10-16 18:45:21 +0800

    Updated data

--- data-raw/create_trans_proportions.R
+++ data-raw/create_trans_proportions.R
@@ -5,16 +5,20 @@ GRCh37.proportions <- list()
 GRCh37.proportions$total.bp <- 3095677412
 
 # size of genic portion of genome
-GRCh37.proportions$coding.bp <- Reduce("+", ICAMS::trans.ranges.GRCh37$end - ICAMS::trans.ranges.GRCh37$start + 1)
+GRCh37.proportions$coding.bp <- 
+  Reduce("+", ICAMS::trans.ranges.GRCh37$end - ICAMS::trans.ranges.GRCh37$start + 1)
 
 # size of the intergenic portion of genome
-GRCh37.proportions$noncoding.bp <- total.bp - coding.bp
+GRCh37.proportions$noncoding.bp <- 
+  GRCh37.proportions$total.bp - GRCh37.proportions$coding.bp
 
 # proportion of genic portion of genome
-GRCh37.proportions$prop.coding <- coding.bp / total.bp
+GRCh37.proportions$prop.coding <- 
+  GRCh37.proportions$coding.bp / GRCh37.proportions$total.bp
 
 # proportion of intergenic portion of genome
-GRCh37.proportions$prop.noncoding <- noncoding.bp / total.bp
+GRCh37.proportions$prop.noncoding <- 
+  GRCh37.proportions$noncoding.bp / GRCh37.proportions$total.bp
 
 
 GRCh38.proportions <- list()
@@ -29,10 +33,13 @@ GRCh38.proportions$total.bp <- 3088269832
 GRCh38.proportions$coding.bp <- Reduce("+", ICAMS::trans.ranges.GRCh38$end - ICAMS::trans.ranges.GRCh38$start + 1)
 
 # size of the intergenic portion of genome
-GRCh38.proportions$noncoding.bp <- total.bp - coding.bp
+GRCh38.proportions$noncoding.bp <- 
+  GRCh38.proportions$total.bp - GRCh38.proportions$coding.bp
 
 # proportion of genic portion of genome
-GRCh38.proportions$prop.coding <- coding.bp / total.bp
+GRCh38.proportions$prop.coding <- 
+  GRCh38.proportions$coding.bp / GRCh38.proportions$total.bp
 
 # proportion of intergenic portion of genome
-GRCh38.proportions$prop.noncoding <- noncoding.bp / total.bp
+GRCh38.proportions$prop.noncoding <- 
+  GRCh38.proportions$noncoding.bp / GRCh38.proportions$total.bp
